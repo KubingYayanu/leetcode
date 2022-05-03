01a079c908aad491befe5ebd7611590351e3ac9f
Kubing Lin <edkt973@gmail.com>
2022-05-04 00:25:28 +0800

    feat: add 1261

---
+++ LeetCode.Library/Medium/1261.FindElementsInAContaminatedBinaryTree.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Library.Medium
+{
+    public class FindElements
+    {
+        public HashSet<int> Set { get; set; } = new HashSet<int>();
+
+        public FindElements(TreeNode root)
+        {
+            Recorver(root, 0);
+        }
+
+        private void Recorver(TreeNode node, int value)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            Set.Add(value);
+            Recorver(node.Left, 2 * value + 1);
+            Recorver(node.Right, 2 * value + 2);
+        }
+
+        public bool Find(int target)
+        {
+            return Set.Contains(target);
+        }
+    }
+
+    public class TreeNode
+    {
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            Value = val;
+            Left = left;
+            Right = right;
+        }
+
+        public int Value { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+    }
+}
---
+++ LeetCode.Testing/Medium/1261.FindElementsInAContaminatedBinaryTreeTest.cs
@@ -0,0 +1,46 @@
+﻿using LeetCode.Library.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Medium
+{
+    [TestFixture]
+    public class FindElementsInAContaminatedBinaryTreeTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void Find_WhenCalled_ReturnBoolean(TestCase testCase)
+        {
+            var sub = new FindElements(testCase.Init);
+
+            var result = sub.Find(testCase.Target);
+
+            Assert.That(testCase.Result, Is.EqualTo(result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Init = new TreeNode(-1, null, new TreeNode(-1)),
+                Target = 1,
+                Result = false
+            };
+            yield return new TestCase
+            {
+                Init = new TreeNode(-1, null, new TreeNode(-1)),
+                Target = 2,
+                Result = true
+            };
+        }
+
+        public class TestCase
+        {
+            public TreeNode Init { get; set; }
+
+            public int Target { get; set; }
+
+            public bool Result { get; set; }
+        }
+    }
+}
