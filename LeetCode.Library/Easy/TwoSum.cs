c908a2b406631e5141bed60fac9a1272c0d20a83
Kubing Lin <edkt973@gmail.com>
2022-05-03 23:13:04 +0800

    init commit

---
+++ LeetCode.Library/Easy/TwoSum.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Library.Easy
+{
+    public class TwoSum
+    {
+        public int[] Run(int[] nums, int target)
+        {
+            var map = new Dictionary<int, int[]>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    var current = nums[i];
+                    var next = nums[j];
+                    var sum = current + next;
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    if (map.ContainsKey(sum))
+                    {
+                        continue;
+                    }
+
+                    map.Add(sum, new int[2] { i, j });
+                }
+            }
+
+            var result = map.First(x => x.Key == target).Value;
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Testing/Easy/TwoSumTest.cs
@@ -0,0 +1,58 @@
+﻿using LeetCode.Library.Easy;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Easy
+{
+    [TestFixture]
+    public class TwoSumTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void Run_WhenCalled_ReturnIndexes(TestCase testCase)
+        {
+            var sub = new TwoSum();
+
+            var indexes = sub.Run(testCase.Nums, testCase.Target);
+
+            CollectionAssert.AreEquivalent(indexes, testCase.Result);
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Nums = new[] { 1, 2, 3 },
+                Target = 3,
+                Result = new[] { 0, 1 }
+            };
+            yield return new TestCase
+            {
+                Nums = new int[] { 2, 7, 11, 15 },
+                Target = 9,
+                Result = new[] { 0, 1 }
+            };
+            yield return new TestCase
+            {
+                Nums = new int[] { 3, 2, 4 },
+                Target = 6,
+                Result = new[] { 1, 2 }
+            };
+            yield return new TestCase
+            {
+                Nums = new int[] { 2, 5, 5, 11 },
+                Target = 10,
+                Result = new[] { 1, 2 }
+            };
+        }
+
+        public class TestCase
+        {
+            public int[] Nums { get; set; }
+
+            public int Target { get; set; }
+
+            public int[] Result { get; set; }
+        }
+    }
+}
