72250a8bc94933adec9fc829b548fc7dde88dab6
Kubing Lin <edkt973@gmail.com>
2022-05-04 21:22:11 +0800

    fix: 修改 Problem 1 的演算法

--- LeetCode.Library/Easy/1.TwoSum.cs
+++ LeetCode.Library/Easy/1.TwoSum.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
+using System.Collections.Generic;
 
 namespace LeetCode.Library.Easy
 {
@@ -7,30 +7,22 @@ namespace LeetCode.Library.Easy
     {
         public int[] Run(int[] nums, int target)
         {
-            var map = new Dictionary<int, int[]>();
-            for (int i = 0; i < nums.Length; i++)
+            var map = new Dictionary<int, int>();
+            for (var i = 0; i < nums.Length; i++)
             {
-                for (int j = 0; j < nums.Length; j++)
+                var complement = target - nums[i];
+                if (map.ContainsKey(complement))
                 {
-                    var current = nums[i];
-                    var next = nums[j];
-                    var sum = current + next;
-                    if (i == j)
-                    {
-                        continue;
-                    }
+                    return new int[] { map[complement], i };
+                }
 
-                    if (map.ContainsKey(sum))
-                    {
-                        continue;
-                    }
-
-                    map.Add(sum, new int[2] { i, j });
+                if (!map.ContainsKey(nums[i]))
+                {
+                    map.Add(nums[i], i);
                 }
             }
 
-            var result = map.First(x => x.Key == target).Value;
-            return result;
+            throw new Exception("No two sum solution");
         }
     }
 }
