7903b3b3569a0936c0ceb6bf832bb31758690e70
Kubing Lin <edkt973@gmail.com>
2022-05-05 19:16:24 +0800

    feat: add Problem 2

---
+++ LeetCode.Library/Medium/0002.AddTwoNumbersSolution.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode.Library.Medium
+{
+    public class AddTwoNumbersSolution
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            var root = new ListNode();
+            var tempNode = root;
+            var carry = 0;
+            while (l1 != null || l2 != null)
+            {
+                var currentSum = 0;
+                if (l1 != null)
+                {
+                    currentSum += l1.val;
+                    l1 = l1.next;
+                }
+
+                if (l2 != null)
+                {
+                    currentSum += l2.val;
+                    l2 = l2.next;
+                }
+
+                currentSum += carry;
+                carry = currentSum / 10;
+
+                tempNode.next = new ListNode(currentSum % 10);
+                tempNode = tempNode.next;
+            }
+
+            if (carry > 0)
+            {
+                tempNode.next = new ListNode(carry);
+            }
+
+            return root.next;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
---
+++ LeetCode.Testing/Medium/0002.AddTwoNumbersSolutionTest.cs
@@ -0,0 +1,69 @@
+﻿using LeetCode.Library.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Medium
+{
+    [TestFixture]
+    public class AddTwoNumbersSolutionTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void Run_WhenCalled_ReturnListNode(TestCase testCase)
+        {
+            var sub = new AddTwoNumbersSolution();
+
+            var result = sub.AddTwoNumbers(testCase.L1, testCase.L2);
+            var areIdentical = AreIdentical(result, testCase.Result);
+
+            Assert.That(areIdentical, Is.True);
+        }
+
+        public bool AreIdentical(ListNode l1, ListNode l2)
+        {
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val != l2.val)
+                {
+                    return false;
+                }
+
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            return (l1 == null && l2 == null);
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            //yield return new TestCase
+            //{
+            //    L1 = new ListNode(2, new ListNode(4, new ListNode(3))),
+            //    L2 = new ListNode(5, new ListNode(6, new ListNode(4))),
+            //    Result = new ListNode(7, new ListNode(0, new ListNode(8))),
+            //};
+            //yield return new TestCase
+            //{
+            //    L1 = new ListNode(),
+            //    L2 = new ListNode(),
+            //    Result = new ListNode(),
+            //};
+            yield return new TestCase
+            {
+                L1 = new ListNode(9),
+                L2 = new ListNode(1, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9, new ListNode(9)))))))))),
+                Result = new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(0, new ListNode(1)))))))))))
+            };
+        }
+
+        public class TestCase
+        {
+            public ListNode L1 { get; set; }
+
+            public ListNode L2 { get; set; }
+
+            public ListNode Result { get; set; }
+        }
+    }
+}
