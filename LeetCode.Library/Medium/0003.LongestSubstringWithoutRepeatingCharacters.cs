aa313034055f0f06788326e7f43009c013caa59f
Kubing Lin <edkt973@gmail.com>
2022-05-05 23:23:25 +0800

    feat: add Problem 3

---
+++ LeetCode.Library/Medium/0003.LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Library.Medium
+{
+    public class LongestSubstringWithoutRepeatingCharacters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            var set = new HashSet<char>();
+            var maxLength = 0;
+            var windowStart = 0;
+            int windowEnd = 0;
+            while (windowEnd < s.Length)
+            {
+                var target = s[windowEnd];
+                if (!set.Contains(target))
+                {
+                    set.Add(target);
+                    maxLength = Math.Max(maxLength, windowEnd - windowStart + 1);
+                    windowEnd++;
+                }
+                else
+                {
+                    set.Remove(s[windowStart]);
+                    windowStart++;
+                }
+            }
+
+            return maxLength;
+        }
+    }
+}
---
+++ LeetCode.Testing/Medium/0003.LongestSubstringWithoutRepeatingCharactersTest.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode.Library.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Medium
+{
+    [TestFixture]
+    public class LongestSubstringWithoutRepeatingCharactersTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void LengthOfLongestSubstring_WhenCalled_ReturnLength(TestCase testCase)
+        {
+            var sut = new LongestSubstringWithoutRepeatingCharacters();
+
+            var result = sut.LengthOfLongestSubstring(testCase.String);
+
+            Assert.That(result, Is.EqualTo(testCase.Result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                String = "abcabcbb",
+                Result = 3
+            };
+            yield return new TestCase
+            {
+                String = "bbbbb",
+                Result = 1
+            };
+            yield return new TestCase
+            {
+                String = "pwwkew",
+                Result = 3
+            };
+        }
+
+        public class TestCase
+        {
+            public string String { get; set; }
+
+            public int Result { get; set; }
+        }
+    }
+}
