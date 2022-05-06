006b394efe9fba02244e42b9591040151a3e1ca9
Kubing Lin <edkt973@gmail.com>
2022-05-06 21:27:04 +0800

    feat: add Problem 4

---
+++ LeetCode.Library/Hard/0004.MedianOfTwoSortedArrays.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace LeetCode.Library.Hard
+{
+    public class MedianOfTwoSortedArrays
+    {
+        public double FindMedianSortedArrays(int[] nums1, int[] nums2)
+        {
+            var totalSize = nums1.Length + nums2.Length;
+            var halfSize = totalSize / 2;
+            int[] aArray, bArray;
+            if (nums1.Length > nums2.Length)
+            {
+                aArray = nums2;
+                bArray = nums1;
+            }
+            else
+            {
+                aArray = nums1;
+                bArray = nums2;
+            }
+
+            var leftPointer = 0;
+            var rightPointer = aArray.Length - 1;
+            while (true)
+            {
+                var aPartitionIndex = (int)Math.Floor((double)(leftPointer + rightPointer) / 2);
+                var bPartitionIndex = halfSize - aPartitionIndex - 2;
+
+                var aLeftValue = aPartitionIndex >= 0
+                    ? aArray[aPartitionIndex] : double.NegativeInfinity;
+                var aRightValue = aPartitionIndex + 1 < aArray.Length
+                    ? aArray[aPartitionIndex + 1] : double.PositiveInfinity;
+                var bLeftValue = bPartitionIndex >= 0
+                    ? bArray[bPartitionIndex] : double.NegativeInfinity;
+                var bRightValue = bPartitionIndex + 1 < bArray.Length
+                    ? bArray[bPartitionIndex + 1] : double.PositiveInfinity;
+
+                var isMedian = aLeftValue <= bRightValue
+                    && bLeftValue <= aRightValue;
+                if (isMedian)
+                {
+                    if (totalSize % 2 == 0)
+                    {
+                        return (Math.Max(aLeftValue, bLeftValue) + Math.Min(aRightValue, bRightValue)) / 2;
+                    }
+                    else
+                    {
+                        return Math.Min(aRightValue, bRightValue);
+                    }
+                }
+                else if (aLeftValue > bRightValue)
+                {
+                    rightPointer = aPartitionIndex - 1;
+                }
+                else
+                {
+                    leftPointer = aPartitionIndex + 1;
+                }
+            }
+        }
+    }
+}
---
+++ LeetCode.Testing/Hard/0004.MedianOfTwoSortedArraysTest.cs
@@ -0,0 +1,58 @@
+﻿using LeetCode.Library.Hard;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Hard
+{
+    [TestFixture]
+    public class MedianOfTwoSortedArraysTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void FindMedianSortedArrays_WhenCalled_ReturnIndexes(TestCase testCase)
+        {
+            var sut = new MedianOfTwoSortedArrays();
+
+            var result = sut.FindMedianSortedArrays(testCase.Nums1, testCase.Nums2);
+
+            Assert.That(result, Is.EqualTo(testCase.Result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Nums1 = new[] { 1, 3 },
+                Nums2 = new[] { 2 },
+                Result = 2
+            };
+            yield return new TestCase
+            {
+                Nums1 = new[] { 1, 2 },
+                Nums2 = new[] { 3, 4 },
+                Result = 2.5
+            };
+            yield return new TestCase
+            {
+                Nums1 = new int[0],
+                Nums2 = new[] { 1 },
+                Result = 1
+            };
+            yield return new TestCase
+            {
+                Nums1 = new[] { 1, 3 },
+                Nums2 = new[] { 2, 7 },
+                Result = 2.5
+            };
+        }
+
+        public class TestCase
+        {
+            public int[] Nums1 { get; set; }
+
+            public int[] Nums2 { get; set; }
+
+            public double Result { get; set; }
+        }
+    }
+}
