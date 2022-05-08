7857326fba7bcd30eb1d540695d78f724ae00423
Kubing Lin <edkt973@gmail.com>
2022-05-08 15:02:54 +0800

    feat: add Problem 7

---
+++ LeetCode.Library/Medium/0007.ReverseInteger.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Library.Medium
+{
+    public class ReverseInteger
+    {
+        public int Reverse(int x)
+        {
+            var sign = 1;
+            if (x < 0)
+            {
+                sign = -1;
+            }
+
+            var source = x * sign;
+            var result = 0d;
+            while (source > 0)
+            {
+                var remainder = (int)source % 10;
+                result = result * 10 + remainder;
+                if (result >= int.MaxValue)
+                {
+                    return 0;
+                }
+
+                source /= 10;
+            }
+
+            return (int)result * sign;
+        }
+    }
+}
---
+++ LeetCode.Testing/Medium/0007.ReverseIntegerTest.cs
@@ -0,0 +1,62 @@
+﻿using LeetCode.Library.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Medium
+{
+    [TestFixture]
+    public class ReverseIntegerTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void Reverse_WhenCalled_ReturnIndexes(TestCase testCase)
+        {
+            var sut = new ReverseInteger();
+
+            var result = sut.Reverse(testCase.Num);
+
+            Assert.That(result, Is.EqualTo(testCase.Result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Num = 0,
+                Result = 0
+            };
+            yield return new TestCase
+            {
+                Num = 123,
+                Result = 321
+            };
+            yield return new TestCase
+            {
+                Num = -123,
+                Result = -321
+            };
+            yield return new TestCase
+            {
+                Num = 120,
+                Result = 21
+            };
+            yield return new TestCase
+            {
+                Num = -2147483648,
+                Result = 0
+            };
+            yield return new TestCase
+            {
+                Num = 1534236469,
+                Result = 0
+            };
+        }
+
+        public class TestCase
+        {
+            public int Num { get; set; }
+
+            public int Result { get; set; }
+        }
+    }
+}
