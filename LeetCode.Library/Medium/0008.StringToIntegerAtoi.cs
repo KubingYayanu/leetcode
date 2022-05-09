05614973f7606b1732b4f4d1de02a97d2e75f1c9
Kubing Lin <edkt973@gmail.com>
2022-05-09 15:06:18 +0800

    feat: add Problem 8

---
+++ LeetCode.Library/Medium/0008.StringToIntegerAtoi.cs
@@ -0,0 +1,56 @@
+﻿namespace LeetCode.Library.Medium
+{
+    public class StringToIntegerAtoi
+    {
+        public int MyAtoi(string s)
+        {
+            var source = s.Trim();
+            if (source.Length == 0)
+            {
+                return 0;
+            }
+
+            var result = 0d;
+            var sign = 1;
+            for (int i = 0; i < source.Length; i++)
+            {
+                var digit = source[i];
+                var isSingned = digit == '-' || digit == '+';
+                if (i != 0 && isSingned)
+                {
+                    break;
+                }
+
+                if (digit == '-')
+                {
+                    sign = -1;
+                }
+
+                if (char.IsNumber(digit))
+                {
+                    result = result * 10 + (int)char.GetNumericValue(digit);
+                }
+                else if (i == 0 && isSingned)
+                {
+                    continue;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            result *= sign;
+            if (result > int.MaxValue)
+            {
+                return int.MaxValue;
+            }
+            else if (result < int.MinValue)
+            {
+                return int.MinValue;
+            }
+
+            return (int)result;
+        }
+    }
+}
---
+++ LeetCode.Testing/Medium/0008.StringToIntegerAtoiTest.cs
@@ -0,0 +1,67 @@
+﻿using LeetCode.Library.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Medium
+{
+    [TestFixture]
+    public class StringToIntegerAtoiTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void MyAtoi_WhenCalled_ReturnIndexes(TestCase testCase)
+        {
+            var sut = new StringToIntegerAtoi();
+
+            var result = sut.MyAtoi(testCase.String);
+
+            Assert.That(result, Is.EqualTo(testCase.Result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            //yield return new TestCase
+            //{
+            //    String = "   -42",
+            //    Result = -42
+            //};
+            //yield return new TestCase
+            //{
+            //    String = "4193 with words",
+            //    Result = 4193
+            //};
+            //yield return new TestCase
+            //{
+            //    String = "+1",
+            //    Result = 1
+            //};
+            //yield return new TestCase
+            //{
+            //    String = "+-12",
+            //    Result = 0
+            //};
+            //yield return new TestCase
+            //{
+            //    String = "00000-42a1234",
+            //    Result = 0
+            //};
+            yield return new TestCase
+            {
+                String = "  -0012a42",
+                Result = -12
+            };
+            yield return new TestCase
+            {
+                String = " b11228552307",
+                Result = 0
+            };
+        }
+
+        public class TestCase
+        {
+            public string String { get; set; }
+
+            public int Result { get; set; }
+        }
+    }
+}
