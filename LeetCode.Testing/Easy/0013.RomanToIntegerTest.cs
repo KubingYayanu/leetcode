2de2a493e31e5ac54edb74ecb420d660cd474d8a
Kubing Lin <edkt973@gmail.com>
2022-05-11 21:11:29 +0800

    feat: add Problem 13

---
+++ LeetCode.Testing/Easy/0013.RomanToIntegerTest.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode.Library.Easy;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Easy
+{
+    [TestFixture]
+    public class RomanToIntegerTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void RomanToInt_WhenCalled_ReturnSum(TestCase testCase)
+        {
+            var sut = new RomanToInteger();
+
+            var result = sut.RomanToInt(testCase.String);
+
+            Assert.That(result, Is.EqualTo(testCase.Result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            //yield return new TestCase
+            //{
+            //    String = "III",
+            //    Result = 3
+            //};
+            //yield return new TestCase
+            //{
+            //    String = "LVIII",
+            //    Result = 58
+            //};
+            yield return new TestCase
+            {
+                String = "MCMXCIV",
+                Result = 1994
+            };
+        }
+
+        public class TestCase
+        {
+            public string String { get; set; }
+
+            public int Result { get; set; }
+        }
+    }
+}
