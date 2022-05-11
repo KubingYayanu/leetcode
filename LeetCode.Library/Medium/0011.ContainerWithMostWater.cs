14ab0d5c1ad3cf3f8acfbbad6a10952461870480
Kubing Lin <edkt973@gmail.com>
2022-05-11 18:09:31 +0800

    feat: add Problem 11

---
+++ LeetCode.Library/Medium/0011.ContainerWithMostWater.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LeetCode.Library.Medium
+{
+    public class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            var l = 0;
+            var r = height.Length - 1;
+            var maxArea = 0;
+            while (l < r)
+            {
+                var area = Math.Min(height[l], height[r]) * (r - l);
+                maxArea = Math.Max(maxArea, area);
+                if (height[l] < height[r])
+                {
+                    l++;
+                }
+                else
+                {
+                    r--;
+                }
+            }
+            return maxArea;
+        }
+    }
+}
---
+++ LeetCode.Testing/Medium/0011.ContainerWithMostWaterTest.cs
@@ -0,0 +1,42 @@
+﻿using LeetCode.Library.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Medium
+{
+    [TestFixture]
+    public class ContainerWithMostWaterTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void MaxArea_WhenCalled_ReturnArea(TestCase testCase)
+        {
+            var sut = new ContainerWithMostWater();
+
+            var result = sut.MaxArea(testCase.Array);
+
+            Assert.That(result, Is.EqualTo(testCase.Result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Array = new[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 },
+                Result = 49
+            };
+            yield return new TestCase
+            {
+                Array = new[] { 1, 1 },
+                Result = 1
+            };
+        }
+
+        public class TestCase
+        {
+            public int[] Array { get; set; }
+
+            public int Result { get; set; }
+        }
+    }
+}
