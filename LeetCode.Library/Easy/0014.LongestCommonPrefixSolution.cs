9909b679904340085c47942209828c021125c8ea
Kubing Lin <edkt973@gmail.com>
2022-05-12 17:51:09 +0800

    feat: add Problem 14

---
+++ LeetCode.Library/Easy/0014.LongestCommonPrefixSolution.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Library.Easy
+{
+    public class LongestCommonPrefixSolution
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            var index = 0;
+            char currentChar = char.MinValue;
+            var stop = false;
+            var array = new List<char>();
+            while (!stop)
+            {
+                for (int i = 0; i < strs.Length; i++)
+                {
+                    var str = strs[i];
+                    if (index < str.Length)
+                    {
+                        if (currentChar == char.MinValue)
+                        {
+                            currentChar = str[index];
+                        }
+                        else if (currentChar == str[index])
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            stop = true;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        stop = true;
+                        break;
+                    }
+                }
+
+                if (!stop)
+                {
+                    array.Add(currentChar);
+                    currentChar = char.MinValue;
+                    index++;
+                }
+            }
+
+            var strArray = array.ToArray();
+            return new string(strArray);
+        }
+    }
+}
---
+++ LeetCode.Testing/Easy/0014.LongestCommonPrefixSolutionTest.cs
@@ -0,0 +1,62 @@
+﻿using LeetCode.Library.Easy;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Easy
+{
+    [TestFixture]
+    public class LongestCommonPrefixSolutionTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void LongestCommonPrefix_WhenCalled_ReturnResult(TestCase testCase)
+        {
+            var sut = new LongestCommonPrefixSolution();
+
+            var result = sut.LongestCommonPrefix(testCase.Array);
+
+            Assert.That(result, Is.EqualTo(testCase.Result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Array = new[] { "flower", "flow", "flight" },
+                Result = "fl"
+            };
+            yield return new TestCase
+            {
+                Array = new[] { "dog", "racecar", "car" },
+                Result = ""
+            };
+            yield return new TestCase
+            {
+                Array = new[] { "" },
+                Result = ""
+            };
+            yield return new TestCase
+            {
+                Array = new[] { "a" },
+                Result = "a"
+            };
+            yield return new TestCase
+            {
+                Array = new[] { "a", "b" },
+                Result = ""
+            };
+            yield return new TestCase
+            {
+                Array = new[] { "ab", "a" },
+                Result = "a"
+            };
+        }
+
+        public class TestCase
+        {
+            public string[] Array { get; set; }
+
+            public string Result { get; set; }
+        }
+    }
+}
