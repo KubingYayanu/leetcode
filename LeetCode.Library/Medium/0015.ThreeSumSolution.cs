dc179f05a9e18df9d3fda48b235862040c1d251f
Kubing Lin <edkt973@gmail.com>
2022-05-13 21:55:08 +0800

    feat: add Problem 15

---
+++ LeetCode.Library/Medium/0015.ThreeSumSolution.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LeetCode.Library.Medium
+{
+    public class ThreeSumSolution
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            var result = new List<IList<int>>();
+            // origin: -3, 3, 4, -3, 1, 2
+            // sorted: -3, -3, 1, 2, 3, 4
+            var source = nums.OrderBy(x => x).ToList();
+            for (int i = 0; i < source.Count; i++)
+            {
+                var target = source[i];
+                // 如果跟前一個數字一樣, 表示有重複的組合
+                if (i > 0 && target == source[i - 1])
+                {
+                    continue;
+                }
+
+                var leftPointer = i + 1;
+                var rightPointer = source.Count - 1;
+                while (leftPointer < rightPointer)
+                {
+                    var threeSum = target + source[leftPointer] + source[rightPointer];
+                    if (threeSum > 0)
+                    {
+                        rightPointer--;
+                    }
+                    else if (threeSum < 0)
+                    {
+                        leftPointer++;
+                    }
+                    else
+                    {
+                        result.Add(new List<int> { target, source[leftPointer], source[rightPointer] });
+                        leftPointer++;
+                        while (source[leftPointer] == source[leftPointer - 1] && leftPointer < rightPointer)
+                        {
+                            leftPointer++;
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Testing/Medium/0015.ThreeSumSolutionTest.cs
@@ -0,0 +1,51 @@
+﻿using LeetCode.Library.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Medium
+{
+    [TestFixture]
+    public class ThreeSumSolutionTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void ThreeSum_WhenCalled_ReturnResult(TestCase testCase)
+        {
+            var sut = new ThreeSumSolution();
+
+            var result = sut.ThreeSum(testCase.Nums);
+
+            CollectionAssert.AreEquivalent(result, testCase.Result);
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Nums = new[] { -1, 0, 1, 2, -1, -4 },
+                Result = new List<IList<int>>
+                {
+                    new List<int> { -1, -1, 2 },
+                    new List<int> { -1, 0, 1 }
+                }
+            };
+            yield return new TestCase
+            {
+                Nums = new int[0],
+                Result = new List<IList<int>>()
+            };
+            yield return new TestCase
+            {
+                Nums = new[] { 0 },
+                Result = new List<IList<int>>()
+            };
+        }
+
+        public class TestCase
+        {
+            public int[] Nums { get; set; }
+
+            public IList<IList<int>> Result { get; set; }
+        }
+    }
+}
