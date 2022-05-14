1c3571cdc8b44f816b1e0f1cee3554439cb466fb
Kubing Lin <edkt973@gmail.com>
2022-05-14 23:59:17 +0800

    feat: add Problem 17

---
+++ LeetCode.Library/Medium/0017.LetterCombinationsOfAPhoneNumber.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Library.Medium
+{
+    public class LetterCombinationsOfAPhoneNumber
+    {
+        private Dictionary<char, List<char>> _map = new Dictionary<char, List<char>>
+        {
+            { '2', new List<char> { 'a', 'b', 'c' } },
+            { '3', new List<char> { 'd', 'e', 'f' } },
+            { '4', new List<char> { 'g', 'h', 'i' } },
+            { '5', new List<char> { 'j', 'k', 'l' } },
+            { '6', new List<char> { 'm', 'n', 'o' } },
+            { '7', new List<char> { 'p', 'q', 'r', 's' } },
+            { '8', new List<char> { 't', 'u', 'v' } },
+            { '9', new List<char> { 'w', 'x', 'y', 'z' } }
+        };
+
+        public IList<string> LetterCombinations(string digits)
+        {
+            var result = new List<string>();
+            var temp = new List<string>();
+            if (digits.Length == 0)
+            {
+                return result;
+            }
+            else if (digits.Length > 1)
+            {
+                var combinations = LetterCombinations(digits.Substring(1, digits.Length - 1));
+                temp.AddRange(combinations);
+            }
+            else
+            {
+                temp.Add("");
+            }
+
+            for (int i = 0; i < temp.Count; i++)
+            {
+                var charSet = _map[digits[0]];
+                for (int j = 0; j < charSet.Count; j++)
+                {
+                    result.Add($"{charSet[j]}{temp[i]}");
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Testing/Medium/0017.LetterCombinationsOfAPhoneNumberTest.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode.Library.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Medium
+{
+    [TestFixture]
+    public class LetterCombinationsOfAPhoneNumberTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void LetterCombinations_WhenCalled_ReturnResult(TestCase testCase)
+        {
+            var sut = new LetterCombinationsOfAPhoneNumber();
+
+            var indexes = sut.LetterCombinations(testCase.String);
+
+            CollectionAssert.AreEquivalent(indexes, testCase.Result);
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                String = "23",
+                Result = new[] { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" }
+            };
+            yield return new TestCase
+            {
+                String = "",
+                Result = new string[0]
+            };
+            yield return new TestCase
+            {
+                String = "2",
+                Result = new[] { "a", "b", "c" }
+            };
+        }
+
+        public class TestCase
+        {
+            public string String { get; set; }
+
+            public string[] Result { get; set; }
+        }
+    }
+}
