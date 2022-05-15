55fb31dfdba1d5cb7ca359cf15ce418adb1db3a7
Kubing Lin <edkt973@gmail.com>
2022-05-15 21:58:21 +0800

    feat: add Problem 20

---
+++ LeetCode.Library/Easy/0020.ValidParentheses.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Library.Easy
+{
+    public class ValidParentheses
+    {
+        public bool IsValid(string s)
+        {
+            if (s.Length == 0)
+            {
+                return false;
+            }
+
+            var stack = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(' || s[i] == '{' || s[i] == '[')
+                {
+                    stack.Push(s[i]);
+                }
+                else if (stack.Count != 0)
+                {
+                    if ((s[i] == ')' && stack.Peek() == '(')
+                        || (s[i] == '}' && stack.Peek() == '{')
+                        || (s[i] == ']' && stack.Peek() == '['))
+                    {
+                        stack.Pop();
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            if (stack.Count != 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ LeetCode.Testing/Easy/0020.ValidParenthesesTest.cs
@@ -0,0 +1,67 @@
+﻿using LeetCode.Library.Easy;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Easy
+{
+    [TestFixture]
+    public class ValidParenthesesTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void IsValid_WhenCalled_ReturnIndexes(TestCase testCase)
+        {
+            var sut = new ValidParentheses();
+
+            var resutl = sut.IsValid(testCase.String);
+
+            Assert.That(resutl, Is.EqualTo(testCase.Result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                String = "(){}}{",
+                Result = false
+            };
+            yield return new TestCase
+            {
+                String = "{[]}",
+                Result = true
+            };
+            yield return new TestCase
+            {
+                String = "()",
+                Result = true
+            };
+            yield return new TestCase
+            {
+                String = "([]){}",
+                Result = true
+            };
+            yield return new TestCase
+            {
+                String = "()[]{}",
+                Result = true
+            };
+            yield return new TestCase
+            {
+                String = "(]",
+                Result = false
+            };
+            yield return new TestCase
+            {
+                String = "",
+                Result = false
+            };
+        }
+
+        public class TestCase
+        {
+            public string String { get; set; }
+
+            public bool Result { get; set; }
+        }
+    }
+}
