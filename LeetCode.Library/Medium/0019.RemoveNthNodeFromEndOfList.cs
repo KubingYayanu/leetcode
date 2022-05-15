00c2516b18fbf18cad4cd48dd1890f3bc3e7757f
Kubing Lin <edkt973@gmail.com>
2022-05-15 19:26:50 +0800

    feat: add Problem 19

---
+++ LeetCode.Library/Medium/0019.RemoveNthNodeFromEndOfList.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Library.Medium
+{
+    public class RemoveNthNodeFromEndOfList
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            var index = 1;
+            var map = new Dictionary<int, ListNode>();
+            while (head != null)
+            {
+                map.Add(index, head);
+                head = head.next;
+                index++;
+            }
+
+            var targetIndex = index - n;
+            var newNextIndex = targetIndex + 1;
+            if (targetIndex == 1 && !map.ContainsKey(newNextIndex))
+            {
+                map[1] = null;
+            }
+            else if (targetIndex == 1 && map.ContainsKey(newNextIndex))
+            {
+                map[1] = map[newNextIndex];
+            }
+            else if (targetIndex == index - 1)
+            {
+                map[targetIndex - 1].next = null;
+            }
+            else if (!map.ContainsKey(newNextIndex))
+            {
+                map[targetIndex].next = null;
+            }
+            else
+            {
+                map[targetIndex - 1].next = map[newNextIndex];
+            }
+
+            return map[1];
+        }
+
+        public class ListNode
+        {
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+
+            public int val { get; set; }
+
+            public ListNode next { get; set; }
+        }
+    }
+}
---
+++ LeetCode.Testing/Medium/0019.RemoveNthNodeFromEndOfListTest.cs
@@ -0,0 +1,76 @@
+﻿using LeetCode.Library.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+using static LeetCode.Library.Medium.RemoveNthNodeFromEndOfList;
+
+namespace LeetCode.Testing.Medium
+{
+    [TestFixture]
+    public class RemoveNthNodeFromEndOfListTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void RemoveNthFromEnd_WhenCalled_ReturnResult(TestCase testCase)
+        {
+            var sut = new RemoveNthNodeFromEndOfList();
+
+            var result = sut.RemoveNthFromEnd(testCase.Head, testCase.Target);
+            var areIdentical = AreIdentical(result, testCase.Result);
+
+            Assert.That(areIdentical, Is.True);
+        }
+
+        public bool AreIdentical(ListNode l1, ListNode l2)
+        {
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val != l2.val)
+                {
+                    return false;
+                }
+
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            return (l1 == null && l2 == null);
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))),
+                Target = 5,
+                Result = new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))
+            };
+            yield return new TestCase
+            {
+                Head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))),
+                Target = 1,
+                Result = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4))))
+            };
+            yield return new TestCase
+            {
+                Head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))),
+                Target = 2,
+                Result = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(5))))
+            };
+            yield return new TestCase
+            {
+                Head = new ListNode(1),
+                Target = 1,
+                Result = null
+            };
+        }
+
+        public class TestCase
+        {
+            public ListNode Head { get; set; }
+
+            public int Target { get; set; }
+
+            public ListNode Result { get; set; }
+        }
+    }
+}
