973828c2f5196150e4c90a9b40fe62faf6eed6d1
Kubing Lin <edkt973@gmail.com>
2022-05-16 21:13:19 +0800

    feat: add Problem 21

---
+++ LeetCode.Library/Easy/0021.MergeTwoSortedLists.cs
@@ -0,0 +1,62 @@
+﻿namespace LeetCode.Library.Easy
+{
+    public class MergeTwoSortedLists
+    {
+        public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+        {
+            ListNode root = null;
+            ListNode last = null;
+            while (list1 != null || list2 != null)
+            {
+                var a = double.PositiveInfinity;
+                var b = double.PositiveInfinity;
+                if (list1 != null)
+                {
+                    a = list1.val;
+                }
+                if (list2 != null)
+                {
+                    b = list2.val;
+                }
+
+                if (a > b)
+                {
+                    var temp = b;
+                    b = a;
+                    a = temp;
+                    list2 = list2.next;
+                }
+                else
+                {
+                    list1 = list1.next;
+                }
+
+                if (root == null)
+                {
+                    root = new ListNode((int)a);
+                    last = root;
+                }
+                else
+                {
+                    last.next = new ListNode((int)a);
+                    last = last.next;
+                }
+            }
+
+            return root;
+        }
+    }
+
+    public class ListNode
+    {
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+
+        public int val { get; set; }
+
+        public ListNode next { get; set; }
+    }
+}
---
+++ LeetCode.Testing/Easy/0021.MergeTwoSortedListsTest.cs
@@ -0,0 +1,69 @@
+﻿using LeetCode.Library.Easy;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Easy
+{
+    [TestFixture]
+    public class MergeTwoSortedListsTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void MergeTwoLists_WhenCalled_ReturnIndexes(TestCase testCase)
+        {
+            var sut = new MergeTwoSortedLists();
+
+            var result = sut.MergeTwoLists(testCase.List1, testCase.List2);
+            var areIdentical = AreIdentical(result, testCase.Result);
+
+            Assert.That(areIdentical, Is.True);
+        }
+
+        public bool AreIdentical(ListNode l1, ListNode l2)
+        {
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val != l2.val)
+                {
+                    return false;
+                }
+
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            return (l1 == null && l2 == null);
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                List1 = new ListNode(1, new ListNode(2, new ListNode(4))),
+                List2 = new ListNode(1, new ListNode(3, new ListNode(4))),
+                Result = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4))))))
+            };
+            yield return new TestCase
+            {
+                List1 = null,
+                List2 = null,
+                Result = null
+            };
+            yield return new TestCase
+            {
+                List1 = null,
+                List2 = new ListNode(0),
+                Result = new ListNode(0)
+            };
+        }
+
+        public class TestCase
+        {
+            public ListNode List1 { get; set; }
+
+            public ListNode List2 { get; set; }
+
+            public ListNode Result { get; set; }
+        }
+    }
+}
