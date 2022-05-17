8a87cc309f21d060eef0d65ca6f3f1c64509febb
Kubing Lin <edkt973@gmail.com>
2022-05-17 14:42:58 +0800

    feat: add Problem 22

---
+++ LeetCode.Library/Medium/0022.GenerateParenthesesSolution.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Library.Medium
+{
+    public class GenerateParenthesesSolution
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            var parenthesis = "()";
+            var result = new List<string> { "" };
+            var index = 0;
+            while (index < n)
+            {
+                var temp = new List<string>();
+                var count = result.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    var str = result[i];
+                    var strLength = str.Length;
+                    for (int j = 0; j < strLength + 1; j++)
+                    {
+                        var newStr = str.Insert(j, parenthesis);
+                        if (!temp.Contains(newStr))
+                        {
+                            temp.Add(newStr);
+                        }
+                    }
+                }
+                result = temp;
+                index++;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Testing/Medium/0022.GenerateParenthesesSolutionTest.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode.Library.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Medium
+{
+    [TestFixture]
+    public class GenerateParenthesesSolutionTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void GenerateParenthesis_WhenCalled_ReturnIndexes(TestCase testCase)
+        {
+            var sut = new GenerateParenthesesSolution();
+
+            var result = sut.GenerateParenthesis(testCase.Count);
+
+            CollectionAssert.AreEquivalent(result, testCase.Result);
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Count = 3,
+                Result = new List<string> { "((()))", "(()())", "(())()", "()(())", "()()()" }
+            };
+            yield return new TestCase
+            {
+                Count = 2,
+                Result = new List<string> { "()()", "(())" }
+            };
+            yield return new TestCase
+            {
+                Count = 1,
+                Result = new List<string> { "()" }
+            };
+        }
+
+        public class TestCase
+        {
+            public int Count { get; set; }
+
+            public IList<string> Result { get; set; }
+        }
+    }
+}
