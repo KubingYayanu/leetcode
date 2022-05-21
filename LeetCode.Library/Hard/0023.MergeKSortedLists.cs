b7167ffd4603b3b8a60f6e4334cdd8ba081b2968
Kubing Lin <edkt973@gmail.com>
2022-05-21 17:07:55 +0800

    feat: add Problem 23

---
+++ LeetCode.Library/Hard/0023.MergeKSortedLists.cs
@@ -0,0 +1,64 @@
+﻿namespace LeetCode.Library.Hard
+{
+    public class MergeKSortedLists
+    {
+        public ListNode MergeKLists(ListNode[] lists)
+        {
+            ListNode root = null;
+            ListNode last = null;
+            var stop = false;
+            while (!stop)
+            {
+                stop = true;
+                var minValueIndex = 0;
+                var minValue = double.PositiveInfinity;
+                for (int i = 0; i < lists.Length; i++)
+                {
+                    var list = lists[i];
+                    if (list != null)
+                    {
+                        stop = false;
+                        var value = list.val;
+                        if (value < minValue)
+                        {
+                            minValue = value;
+                            minValueIndex = i;
+                        }
+                    }
+                }
+
+                if (stop)
+                {
+                    return root;
+                }
+
+                lists[minValueIndex] = lists[minValueIndex].next;
+                if (root == null)
+                {
+                    root = new ListNode((int)minValue);
+                    last = root;
+                }
+                else
+                {
+                    last.next = new ListNode((int)minValue);
+                    last = last.next;
+                }
+            }
+
+            return root;
+        }
+
+        public class ListNode
+        {
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+
+            public int val { get; set; }
+
+            public ListNode next { get; set; }
+        }
+    }
+}
---
+++ LeetCode.Testing/Hard/0023.MergeKSortedListsTest.cs
@@ -0,0 +1,60 @@
+﻿using LeetCode.Library.Hard;
+using NUnit.Framework;
+using System.Collections.Generic;
+using static LeetCode.Library.Hard.MergeKSortedLists;
+
+namespace LeetCode.Testing.Hard
+{
+    [TestFixture]
+    public class MergeKSortedListsTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void MergeKLists_WhenCalled_ReturnIndexes(TestCase testCase)
+        {
+            var sut = new MergeKSortedLists();
+
+            var result = sut.MergeKLists(testCase.Nodes);
+            var areIdentical = AreIdentical(result, testCase.Result);
+
+            Assert.That(areIdentical, Is.True);
+        }
+
+        public bool AreIdentical(ListNode l1, ListNode l2)
+        {
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val != l2.val)
+                {
+                    return false;
+                }
+
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            return (l1 == null && l2 == null);
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Nodes = new[]
+                {
+                    new ListNode(1, new ListNode(4, new ListNode(5))),
+                    new ListNode(1, new ListNode(3, new ListNode(4))),
+                    new ListNode(2, new ListNode(6))
+                },
+                Result = new ListNode(1, new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(4, new ListNode(5, new ListNode(6))))))))
+            };
+        }
+
+        public class TestCase
+        {
+            public ListNode[] Nodes { get; set; }
+
+            public ListNode Result { get; set; }
+        }
+    }
+}
