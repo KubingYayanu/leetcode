ebe524335fdcf4490bb0b0a35c65e0bd97498348
Kubing Lin <edkt973@gmail.com>
2022-05-23 19:19:58 +0800

    feat: add Problem 26

---
+++ LeetCode.Library/Easy/0026.RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,20 @@
+﻿namespace LeetCode.Library.Easy
+{
+    public class RemoveDuplicatesFromSortedArray
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            var leftPointer = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    nums[leftPointer] = nums[i];
+                    leftPointer++;
+                }
+            }
+
+            return leftPointer;
+        }
+    }
+}
---
+++ LeetCode.Testing/Easy/0026.RemoveDuplicatesFromSortedArrayTest.cs
@@ -0,0 +1,62 @@
+﻿using LeetCode.Library.Easy;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Easy
+{
+    [TestFixture]
+    public class RemoveDuplicatesFromSortedArrayTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void RemoveDuplicates_WhenCalled_ReturnIndexes(TestCase testCase)
+        {
+            var sut = new RemoveDuplicatesFromSortedArray();
+
+            var result = sut.RemoveDuplicates(testCase.Array);
+
+            Assert.That(result, Is.EqualTo(testCase.Result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Array = new[] { 1, 1, 1, 1 },
+                Result = 1
+            };
+            yield return new TestCase
+            {
+                Array = new[] { 1, 1, 1 },
+                Result = 1
+            };
+            yield return new TestCase
+            {
+                Array = new[] { 1, 1 },
+                Result = 1
+            };
+            yield return new TestCase
+            {
+                Array = new[] { 10, 20, 30 },
+                Result = 3
+            };
+            yield return new TestCase
+            {
+                Array = new[] { 1, 1, 2 },
+                Result = 2
+            };
+            yield return new TestCase
+            {
+                Array = new[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 },
+                Result = 5
+            };
+        }
+
+        public class TestCase
+        {
+            public int[] Array { get; set; }
+
+            public int Result { get; set; }
+        }
+    }
+}
