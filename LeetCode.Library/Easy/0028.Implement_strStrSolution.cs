9be29d9742312d510154fd52bc9c6f5dbadeb019
Kubing Lin <edkt973@gmail.com>
2022-05-24 22:39:27 +0800

    add Problem 28

---
+++ LeetCode.Library/Easy/0028.Implement_strStrSolution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Library.Easy
+{
+    public class Implement_strStrSolution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+            if (needle.Length == 0)
+            {
+                return 0;
+            }
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                var same = true;
+                var needlePointer = needle.Length - 1;
+                for (int j = needlePointer; 0 <= j; j--)
+                {
+                    var haystackPointer = i + j;
+                    if (haystackPointer > haystack.Length - 1)
+                    {
+                        return -1;
+                    }
+
+                    if (haystack[haystackPointer] != needle[j])
+                    {
+                        same = false;
+                        break;
+                    }
+                }
+
+                if (same)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode.Testing/Easy/0028.Implement_strStrSolutionTest.cs
@@ -0,0 +1,46 @@
+﻿using LeetCode.Library.Easy;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Easy
+{
+    [TestFixture]
+    public class Implement_strStrSolutionTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void StrStr_WhenCalled_ReturnIndexes(TestCase testCase)
+        {
+            var sut = new Implement_strStrSolution();
+
+            var result = sut.StrStr(testCase.Haystack, testCase.Needle);
+
+            Assert.That(result, Is.EqualTo(testCase.Result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            //yield return new TestCase
+            //{
+            //    Haystack = "hello",
+            //    Needle = "ll",
+            //    Result = 2
+            //};
+            yield return new TestCase
+            {
+                Haystack = "aaaaa",
+                Needle = "bba",
+                Result = -1
+            };
+        }
+
+        public class TestCase
+        {
+            public string Haystack { get; set; }
+
+            public string Needle { get; set; }
+
+            public int Result { get; set; }
+        }
+    }
+}
