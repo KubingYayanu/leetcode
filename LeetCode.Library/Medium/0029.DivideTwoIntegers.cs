10faace4f07c964de5fa0a8b42f0038b8100dd37
Kubing Lin <edkt973@gmail.com>
2022-05-29 01:31:31 +0800

    feat: add Problem 29

---
+++ LeetCode.Library/Medium/0029.DivideTwoIntegers.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace LeetCode.Library.Medium
+{
+    public class DivideTwoIntegers
+    {
+        public int Divide(int dividend, int divisor)
+        {
+            if (divisor == 1)
+            {
+                return dividend;
+            }
+            else if (divisor == -1 && dividend == int.MinValue)
+            {
+                return int.MaxValue;
+            }
+            else if (dividend == divisor)
+            {
+                return 1;
+            }
+            else if (dividend == 0 || divisor == int.MinValue)
+            {
+                return 0;
+            }
+
+            var result = 0;
+            // 避免取 abs 之後 overflow
+            if (dividend == int.MinValue)
+            {
+                if (divisor < 0)
+                {
+                    dividend -= divisor;
+                }
+                else
+                {
+                    dividend += divisor;
+                }
+                result++;
+            }
+
+            var isNegative = (dividend < 0 && divisor > 0) || (dividend > 0 && divisor < 0);
+            var absDividend = Math.Abs(dividend);
+            var absDivisor = Math.Abs(divisor);
+            while (absDividend >= absDivisor)
+            {
+                var temp = absDivisor;
+                var count = 1;
+                // temp << 1 > 0, 避免 temp << 1 overflow
+                while (temp << 1 <= absDividend && temp << 1 > 0)
+                {
+                    temp <<= 1;
+                    count <<= 1;
+                }
+
+                // dividend - (divisor * 2^count) > 0
+                result += count;
+                absDividend -= temp;
+            }
+
+            // ~-result + 1 取代 -1 * result
+            return isNegative ? ~result + 1 : result;
+        }
+    }
+}
---
+++ LeetCode.Testing/Medium/0029.DivideTwoIntegersTest.cs
@@ -0,0 +1,58 @@
+﻿using LeetCode.Library.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Medium
+{
+    [TestFixture]
+    public class DivideTwoIntegersTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void Divide_WhenCalled_ReturnResult(TestCase testCase)
+        {
+            var sut = new DivideTwoIntegers();
+
+            var result = sut.Divide(testCase.Dividend, testCase.Divisor);
+
+            Assert.That(result, Is.EqualTo(testCase.Result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Dividend = 10,
+                Divisor = 3,
+                Result = 3
+            };
+            yield return new TestCase
+            {
+                Dividend = 7,
+                Divisor = -3,
+                Result = -2
+            };
+            yield return new TestCase
+            {
+                Dividend = 2147483647,
+                Divisor = 2,
+                Result = 1073741823
+            };
+            yield return new TestCase
+            {
+                Dividend = -2147483648,
+                Divisor = 2,
+                Result = -1073741824
+            };
+        }
+
+        public class TestCase
+        {
+            public int Dividend { get; set; }
+
+            public int Divisor { get; set; }
+
+            public int Result { get; set; }
+        }
+    }
+}
