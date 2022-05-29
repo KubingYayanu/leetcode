ef3bc5a3d91f91392ea3c85b9aecdd4ef7fcd07a
Kubing Lin <edkt973@gmail.com>
2022-05-29 21:03:11 +0800

    feat: add Problem 33

---
+++ LeetCode.Library/Medium/0033.SearchInRotatedSortedArray.cs
@@ -0,0 +1,44 @@
+﻿namespace LeetCode.Library.Medium
+{
+    public class SearchInRotatedSortedArray
+    {
+        public int Search(int[] nums, int target)
+        {
+            var low = 0;
+            var high = nums.Length - 1;
+            while (low <= high)
+            {
+                var mid = (low + high) / 2;
+                if (nums[mid] == target)
+                {
+                    return mid;
+                }
+
+                if (nums[mid] < nums[high])
+                {
+                    if (target > nums[mid] && target <= nums[high])
+                    {
+                        low = mid + 1;
+                    }
+                    else
+                    {
+                        high = mid - 1;
+                    }
+                }
+                else
+                {
+                    if (target >= nums[low] && target < nums[mid])
+                    {
+                        high = mid - 1;
+                    }
+                    else
+                    {
+                        low = mid + 1;
+                    }
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode.Testing/Medium/0033.SearchInRotatedSortedArrayTest.cs
@@ -0,0 +1,52 @@
+﻿using LeetCode.Library.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Medium
+{
+    [TestFixture]
+    public class SearchInRotatedSortedArrayTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void Search_WhenCalled_ReturnResult(TestCase testCase)
+        {
+            var sut = new SearchInRotatedSortedArray();
+
+            var result = sut.Search(testCase.Array, testCase.Target);
+
+            Assert.That(result, Is.EqualTo(testCase.Result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Array = new int[] { 4, 5, 6, 7, 0, 1, 2 },
+                Target = 0,
+                Result = 4
+            };
+            yield return new TestCase
+            {
+                Array = new int[] { 4, 5, 6, 7, 0, 1, 2 },
+                Target = 3,
+                Result = -1
+            };
+            yield return new TestCase
+            {
+                Array = new int[] { 1, 3 },
+                Target = 3,
+                Result = 1
+            };
+        }
+
+        public class TestCase
+        {
+            public int[] Array { get; set; }
+
+            public int Target { get; set; }
+
+            public int Result { get; set; }
+        }
+    }
+}
