ca629f00c7c6543d527cf65674d08578b7190040
Kubing Lin <edkt973@gmail.com>
2022-05-31 17:50:37 +0800

    feat: add Problem 34

---
+++ LeetCode.Library/Medium/0034.FindFirstAndLastPositionOfElementInSortedArray.cs
@@ -0,0 +1,54 @@
+﻿namespace LeetCode.Library.Medium
+{
+    public class FindFirstAndLastPositionOfElementInSortedArray
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            var result = new int[] { -1, -1 };
+            if (nums.Length == 0)
+            {
+                return result;
+            }
+
+            var left = 0;
+            var right = nums.Length - 1;
+            while (left < right)
+            {
+                var mid = (left + right) / 2;
+                if (nums[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid;
+                }
+            }
+
+            if (nums[left] != target)
+            {
+                return result;
+            }
+            result[0] = left;
+
+            right = nums.Length - 1;
+            while (left < right)
+            {
+                // Make mid biased to the right
+                // So that this won't make the search range stuck.
+                var mid = (left + right) / 2 + 1;
+                if (nums[mid] > target)
+                {
+                    right = mid - 1;
+                }
+                else
+                {
+                    left = mid;
+                }
+            }
+
+            result[1] = right;
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Testing/Medium/0034.FindFirstAndLastPositionOfElementInSortedArrayTest.cs
@@ -0,0 +1,52 @@
+﻿using LeetCode.Library.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Medium
+{
+    [TestFixture]
+    public class FindFirstAndLastPositionOfElementInSortedArrayTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void SearchRange_WhenCalled_ReturnResult(TestCase testCase)
+        {
+            var sut = new FindFirstAndLastPositionOfElementInSortedArray();
+
+            var result = sut.SearchRange(testCase.Array, testCase.Target);
+
+            CollectionAssert.AreEquivalent(result, testCase.Result);
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Array = new int[] { 5, 7, 7, 8, 8, 10 },
+                Target = 8,
+                Result = new int[] { 3, 4 }
+            };
+            yield return new TestCase
+            {
+                Array = new int[] { 5, 7, 7, 8, 8, 10 },
+                Target = 6,
+                Result = new int[] { -1, -1 }
+            };
+            yield return new TestCase
+            {
+                Array = new int[0],
+                Target = 0,
+                Result = new int[] { -1, -1 }
+            };
+        }
+
+        public class TestCase
+        {
+            public int[] Array { get; set; }
+
+            public int Target { get; set; }
+
+            public int[] Result { get; set; }
+        }
+    }
+}
