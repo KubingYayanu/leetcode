4df6836f92af1372921dfb14b3e81864c16f1f28
Kubing Lin <edkt973@gmail.com>
2022-06-06 21:28:08 +0800

    feat: add Problem 36

---
+++ LeetCode.Library/Medium/0036.ValidSudoku.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Library.Medium
+{
+    public class ValidSudoku
+    {
+        public bool IsValidSudoku(char[][] board)
+        {
+            var map = new HashSet<string>();
+            for (int row = 0; row < board.Length; row++)
+            {
+                for (int column = 0; column < board[row].Length; column++)
+                {
+                    var number = board[row][column];
+                    if (number == '.')
+                    {
+                        continue;
+                    }
+                    if (!map.Add($"Number {number} in row {row}")
+                        || !map.Add($"Number {number} in column {column}")
+                        || !map.Add($"Number {number} in block ({row / 3}, {column / 3})"))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCode.Testing/Medium/0036.ValidSudokuTest.cs
@@ -0,0 +1,64 @@
+﻿using LeetCode.Library.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Medium
+{
+    [TestFixture]
+    public class ValidSudokuTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void IsValidSudoku_WhenCalled_ReturnResult(TestCase testCase)
+        {
+            var sut = new ValidSudoku();
+
+            var result = sut.IsValidSudoku(testCase.Board);
+
+            Assert.That(result, Is.EqualTo(testCase.Result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Board = new char[][]
+                {
+                    new char[] { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+                    new char[] { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+                    new char[] { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+                    new char[] { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+                    new char[] { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+                    new char[] { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+                    new char[] { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+                    new char[] { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+                    new char[] { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
+                },
+                Result = true
+            };
+            yield return new TestCase
+            {
+                Board = new char[][]
+                {
+                    new char[] { '8', '3', '.', '.', '7', '.', '.', '.', '.' },
+                    new char[] { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+                    new char[] { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+                    new char[] { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+                    new char[] { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+                    new char[] { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+                    new char[] { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+                    new char[] { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+                    new char[] { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
+                },
+                Result = false
+            };
+        }
+
+        public class TestCase
+        {
+            public char[][] Board { get; set; }
+
+            public bool Result { get; set; }
+        }
+    }
+}
