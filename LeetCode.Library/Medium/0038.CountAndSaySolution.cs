93d76d005ae94f13a19b00f55ae30f20f1983345
Kubing Lin <edkt973@gmail.com>
2022-06-08 21:04:11 +0800

    feat: add Problem 38

---
+++ LeetCode.Library/Medium/0038.CountAndSaySolution.cs
@@ -0,0 +1,39 @@
+﻿namespace LeetCode.Library.Medium
+{
+    public class CountAndSaySolution
+    {
+        public string CountAndSay(int n)
+        {
+            if (n == 1)
+            {
+                return "1";
+            }
+
+            var result = "";
+            var count = 0;
+            var charactor = char.MinValue;
+            var array = CountAndSay(n - 1).ToCharArray();
+            foreach (var c in array)
+            {
+                if (charactor == char.MinValue)
+                {
+                    charactor = c;
+                    count = 1;
+                }
+                else if (charactor != c)
+                {
+                    result += $"{count}{charactor}";
+                    charactor = c;
+                    count = 1;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            result += $"{count}{charactor}";
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Testing/Medium/0038.CountAndSaySolutionTest.cs
@@ -0,0 +1,47 @@
+﻿using LeetCode.Library.Medium;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Medium
+{
+    [TestFixture]
+    public class CountAndSaySolutionTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void CountAndSay_WhenCalled_ReturnResult(TestCase testCase)
+        {
+            var sut = new CountAndSaySolution();
+
+            var result = sut.CountAndSay(testCase.Num);
+
+            Assert.That(result, Is.EqualTo(testCase.Result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Num = 1,
+                Result = "1"
+            };
+            yield return new TestCase
+            {
+                Num = 4,
+                Result = "1211"
+            };
+            yield return new TestCase
+            {
+                Num = 5,
+                Result = "111221"
+            };
+        }
+
+        public class TestCase
+        {
+            public int Num { get; set; }
+
+            public string Result { get; set; }
+        }
+    }
+}
