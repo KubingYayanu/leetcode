c7e4e42a29f067a3b055320d02d1466a71ef3481
Kubing Lin <edkt973@gmail.com>
2022-06-18 19:17:54 +0800

    feat: add Problem 9

---
+++ LeetCode.Library/Easy/0009.PalindromeNumber.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCode.Library.Easy
+{
+    public class PalindromeNumber
+    {
+        public bool IsPalindrome(int x)
+        {
+            var num = x.ToString();
+            var median = num.Length / 2;
+            for (int i = 0; i < median; i++)
+            {
+                if (num[i] != num[num.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ LeetCode.Testing/Easy/0009.PalindromeNumberTest.cs
@@ -0,0 +1,52 @@
+﻿using LeetCode.Library.Easy;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace LeetCode.Testing.Easy
+{
+    [TestFixture]
+    public class PalindromeNumberTest
+    {
+        [Test]
+        [TestCaseSource(nameof(Cases))]
+        public void IsPalindrome_WhenCalled_ReturnResult(TestCase testCase)
+        {
+            var sut = new PalindromeNumber();
+
+            var result = sut.IsPalindrome(testCase.Num);
+
+            Assert.That(result, Is.EqualTo(testCase.Result));
+        }
+
+        private static IEnumerable<TestCase> Cases()
+        {
+            yield return new TestCase
+            {
+                Num = 121,
+                Result = true
+            };
+            yield return new TestCase
+            {
+                Num = -121,
+                Result = false
+            };
+            yield return new TestCase
+            {
+                Num = 10,
+                Result = false
+            };
+            yield return new TestCase
+            {
+                Num = 1001,
+                Result = true
+            };
+        }
+
+        public class TestCase
+        {
+            public int Num { get; set; }
+
+            public bool Result { get; set; }
+        }
+    }
+}
