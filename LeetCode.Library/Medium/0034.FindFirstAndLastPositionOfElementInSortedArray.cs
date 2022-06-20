26869f244c430c41972675c165f34ddd40f60fb3
Kubing Lin <edkt973@gmail.com>
2022-06-20 12:12:31 +0800

    fix: 調整 Problem 34, 使用 pointer 加上 hash table

--- LeetCode.Library/Medium/0034.FindFirstAndLastPositionOfElementInSortedArray.cs
+++ LeetCode.Library/Medium/0034.FindFirstAndLastPositionOfElementInSortedArray.cs
@@ -1,54 +1,28 @@
-﻿namespace LeetCode.Library.Medium
+﻿using System.Collections.Generic;
+
+namespace LeetCode.Library.Medium
 {
     public class FindFirstAndLastPositionOfElementInSortedArray
     {
         public int[] SearchRange(int[] nums, int target)
         {
-            var result = new int[] { -1, -1 };
-            if (nums.Length == 0)
+            var map = new HashSet<int>();
+            var left = -1;
+            var right = -1;
+            for (int i = 0; i < nums.Length; i++)
             {
-                return result;
-            }
-
-            var left = 0;
-            var right = nums.Length - 1;
-            while (left < right)
-            {
-                var mid = (left + right) / 2;
-                if (nums[mid] < target)
+                if (target == nums[i] && map.Add(nums[i]))
                 {
-                    left = mid + 1;
+                    left = i;
+                    right = i;
                 }
-                else
+                else if (target == nums[i] && !map.Add(nums[i]))
                 {
-                    right = mid;
+                    right = i;
                 }
             }
 
-            if (nums[left] != target)
-            {
-                return result;
-            }
-            result[0] = left;
-
-            right = nums.Length - 1;
-            while (left < right)
-            {
-                // Make mid biased to the right
-                // So that this won't make the search range stuck.
-                var mid = (left + right) / 2 + 1;
-                if (nums[mid] > target)
-                {
-                    right = mid - 1;
-                }
-                else
-                {
-                    left = mid;
-                }
-            }
-
-            result[1] = right;
-            return result;
+            return new int[] { left, right };
         }
     }
 }
